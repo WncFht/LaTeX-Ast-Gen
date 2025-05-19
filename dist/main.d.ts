fe423794444548deb0b6276afa821ec927b322f0
WncFht <2130212584@qq.com>
2025-05-19 14:51:01 +0800

    🔧 chore: Update the .gitignore

--- dist/main.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/**
- * 主入口模块 - CLI包装器
- * 命令行工具的程序入口点
- */
-import type * as Ast from '@unified-latex/unified-latex-types';
-export type { Ast };
--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * 类型定义模块
- * 定义LaTeX AST解析器使用的所有TypeScript类型和接口
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
